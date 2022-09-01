047ac96822d6eec322f85cb7c49ba46cf07bce50
Conslatekoyo <conslatekoyo@gmail.com>
2022-09-01 14:25:22 +0300

    first commit

---
+++ src/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App';
+import Login from './Login';
+
+function Login(){
+    return(
+        <div>
+            <h1>Welcome Back</h1>
+            <h2>Your Email</h2>
+            <input>name@domain.com</input>
+            <h2>Password</h2>
+            <input>name@domain.com</input>
+        </div>
+    )
+}
